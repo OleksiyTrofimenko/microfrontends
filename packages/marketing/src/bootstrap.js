65d15f9a0119a7bc1f3a35b02a2d2063685a9107
Oleksiy Trofimenko <gtalehandro@gmail.com>
2021-01-18 22:43:57 +0200

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,24 @@
+// Mount function to start the app
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mount = (el) => {
+  ReactDOM.render(
+    <App />,
+    el
+  );
+}
+
+// Dev, isolation, prod
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#_marketing-dev-root');
+
+  if (el) {
+    mount(el)
+  }
+}
+
+export {
+  mount
+}
