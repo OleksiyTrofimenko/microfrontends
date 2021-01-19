44d08184fd056bdd3777ebf24bfc85fb4315e55b
Oleksiy Trofimenko <gtalehandro@gmail.com>
2021-01-19 23:33:33 +0200

    Container change v1

--- packages/container/components/MarketingApp.js
+++ packages/container/components/MarketingApp.js
@@ -4,6 +4,8 @@ import { mount } from 'marketing/MarketingApp';
 export default () => {
   const ref = useRef(null);
 
+  console.log('this is container change');
+
   useEffect(() => {
     if (ref) {
       mount(ref.current)
