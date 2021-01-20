2a7f411489e39e61036464cc81aa12646a62ee9f
Oleksiy Trofimenko <gtalehandro@gmail.com>
2021-01-20 11:39:58 +0200

    Update marketing branch

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -10,6 +10,8 @@ const mount = (el) => {
   );
 }
 
+console.log('Co co');
+
 // Dev, isolation, prod
 if (process.env.NODE_ENV === 'development') {
   const el = document.querySelector('#_marketing-dev-root');
