50a48297b1f405ef2d6ee046a3ca3945f1202d28
Oleksiy Trofimenko <gtalehandro@gmail.com>
2021-01-21 09:39:37 +0200

    Provide Header

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,23 +4,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const mount = (el) => {
-  ReactDOM.render(
-    <App />,
-    el
-  );
-}
-
-console.log('Co co');
+  ReactDOM.render(<App />, el);
+};
 
 // Dev, isolation, prod
 if (process.env.NODE_ENV === 'development') {
   const el = document.querySelector('#_marketing-dev-root');
 
   if (el) {
-    mount(el)
+    mount(el);
   }
 }
 
-export {
-  mount
-}
+export { mount };
